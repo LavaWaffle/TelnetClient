4f732f6d065b0d858c18807aab23d271fd2cce3d
Hazel <bjxobjxo@gmail.com>
2026-06-25 00:30:15 -0700

    Refactored code splitting up parsing input from processing events

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use bytes::{Buf, BytesMut};
 use std::error::Error;
+use std::io::Read;
 use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
+use std::io::Write;
 
 pub mod TelnetConsts {
     pub const IAC: u8 = 0xFF;
@@ -13,10 +15,134 @@ pub mod TelnetConsts {
     pub const ECHO: u8 = 0x01;
 }
 
+#[derive(Clone, Copy, PartialEq)]
+pub enum NegState {
+    No,       // Option is disabled
+    Yes,      // Option is enabled
+    WantNo,   // We asked to disable, waiting for reply
+    WantYes,  // We asked to enable, waiting for reply
+}
+
+impl Default for NegState {
+    fn default() -> Self {
+        NegState::No
+    }
+}
+
+#[derive(Default, Clone, Copy)]
+pub struct OptionState {
+    pub local: NegState,  // Our state (WILL/WONT)
+    pub remote: NegState, // Their state (DO/DONT)
+}
+
+pub struct TelnetClient {
+    // Option = Idx
+    options: [OptionState; 256],
+}
+
+impl TelnetClient {
+    pub fn new() -> Self {
+        Self {
+            options: [OptionState::default(); 256],
+        }
+    }
+
+    pub fn set_remote(&mut self, idx: u8, state: NegState) {
+        self.options[idx as usize].remote = state;
+    }
+
+    pub fn set_local(&mut self, idx: u8, state: NegState) {
+        self.options[idx as usize].local = state;
+    }
+
+    pub fn is_remote(&self, idx: u8, state: NegState) -> bool {
+        self.options[idx as usize].remote == state
+    }
+    pub fn is_local(&self, idx: u8, state: NegState) -> bool {
+        self.options[idx as usize].local == state
+    }
+}
 enum TelnetState {
-    NormalText,
+    AwaitingIAC,
     AwaitingCmd,
-    AwaitingOpt,
+    AwaitingOpt(u8),
+}
+
+#[derive(Debug)]
+pub enum TelnetEvent {
+    Text(Vec<u8>),
+    Command(u8, u8),     // e.g., (DO, ECHO)
+    Subnegotiation(Vec<u8>),
+}
+
+pub struct TelnetParser {
+    state: TelnetState,
+    text_buffer: Vec<u8>,
+}
+
+impl TelnetParser {
+    pub fn new() -> Self {
+        Self {
+            state: TelnetState::AwaitingIAC,
+            text_buffer: Vec::new(),
+        }
+    }
+
+    #[inline]
+    pub fn push_text_buffer(&mut self, events: &mut Vec<TelnetEvent>) {
+        if !self.text_buffer.is_empty() {
+            let text = std::mem::take(&mut self.text_buffer);
+            events.push(TelnetEvent::Text(text));
+        }
+    }
+
+    pub fn parse_bytes(&mut self, data: &[u8]) -> Vec<TelnetEvent> {
+        let mut events: Vec<TelnetEvent> = Vec::new();
+
+        for &byte in data {
+            match self.state {
+                TelnetState::AwaitingIAC => {
+                    if byte == TelnetConsts::IAC {
+                        self.state = TelnetState::AwaitingCmd;
+                    } else {
+                        self.text_buffer.push(byte);
+                    }
+                }
+
+                TelnetState::AwaitingCmd => {
+                    match byte {
+                        TelnetConsts::DO | TelnetConsts::DONT |
+                        TelnetConsts::WILL | TelnetConsts::WONT => {
+                            self.push_text_buffer(&mut events);
+                            self.state = TelnetState::AwaitingOpt(byte);
+                        }
+                        TelnetConsts::IAC => {
+                            // 0xff 0xff -> 0xff into char stream
+                            self.text_buffer.push(byte);
+                            self.state = TelnetState::AwaitingIAC;
+                        }
+                        _ => {
+                            println!("[TelnetParser] Did not receive IAC or CMD after receiving IAC");
+                            self.push_text_buffer(&mut events);
+                            self.state = TelnetState::AwaitingIAC;
+                        }
+                    }
+                }
+                TelnetState::AwaitingOpt(cmd) => {
+                    events.push(
+                        TelnetEvent::Command(cmd, byte)
+                    );
+
+                    self.state = TelnetState::AwaitingIAC;
+                }
+            }
+        }
+
+        // push any remaining text buffer
+        self.push_text_buffer(&mut events);
+
+        events
+    }
 }
 
 #[tokio::main]
@@ -34,7 +160,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let mut stdin = io::stdin();
     let mut stdin_buffer = [0u8; 1024];
 
-    let mut state = TelnetState::NormalText;
+    let mut state = TelnetState::AwaitingIAC;
+    let mut tel_cli = TelnetClient::new();
+    let mut tel_parser = TelnetParser::new();
 
     loop {
         tokio::select! {
@@ -45,49 +173,44 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     println!("Connection closed by server");
                     break;
                 }
-
-                for (idx, byte) in network_buffer.iter().enumerate() {
-                    if idx >= bytes_read {
-                        break;
-                    }
-
-                    match state {
-                        TelnetState::NormalText => {
-                            match *byte {
-                                TelnetConsts::IAC => {
-                                    state = TelnetState::AwaitingCmd;
-                                    println!("Recv 0xFF moving to Await Cmd State");
-                                }
-                                _ => {
-                                    print!("{}", *byte as char);
-                                }
+                println!("Received {bytes_read} bytes: {:02X?}", &network_buffer[..]);
+                let events = tel_parser.parse_bytes(&network_buffer[..bytes_read]);
+                println!("Formatted into {events:?}");
+                
+                for event in events {
+                    match event {
+                        TelnetEvent::Text(bytes) => {
+                            match std::str::from_utf8(&bytes) {
+                                Ok(valid_str) => {
+                                    print!("{}", valid_str);
+                                    std::io::stdout().flush().unwrap();
+                                },
+                                Err(e) => println!("Invalid UTF-8 sequence: {}", e),
                             }
                         }
-                        TelnetState::AwaitingCmd => {
-                            match *byte {
-                                TelnetConsts::DO => {
-                                    state = TelnetState::AwaitingOpt;
-                                    println!("Recv Cmd: 0xFD (DO) moving to Await Opt State");
+                        TelnetEvent::Command(cmd, opt) => {
+                            if cmd == TelnetConsts::DO {
+                                tel_cli.set_remote(opt, NegState::WantYes);
+
+                                if opt == TelnetConsts::ECHO && !tel_cli.is_local(TelnetConsts::ECHO, NegState::Yes){
+                                    tel_cli.set_local(TelnetConsts::ECHO, NegState::Yes);
+                                    socket_write.write_all(&[TelnetConsts::IAC, TelnetConsts::WILL, opt]).await?;
                                 }
-                                _ => {
-                                    state = TelnetState::NormalText;
-                                    println!("Unrecognized command byte: {:02X}, returning to NormalText", byte);
-                                    state = TelnetState::NormalText;
-                                }
+                            } else if cmd == TelnetConsts::DONT {
+                                tel_cli.set_remote(opt, NegState::WantNo);
+                            } else if cmd == TelnetConsts::WILL {
+                                tel_cli.set_remote(opt, NegState::Yes);
+                            } else if cmd == TelnetConsts::WONT {
+                                tel_cli.set_remote(opt, NegState::No);
                             }
                         }
-                        TelnetState::AwaitingOpt => {
-                            if *byte == TelnetConsts::ECHO {
-                                state = TelnetState::NormalText;
-                                println!("Recv Opt: 0x01 (ECHO) moving to Normal Text State");
-                                socket_write.write_all(&[TelnetConsts::IAC, TelnetConsts::WILL, TelnetConsts::ECHO]).await?;
-                            }
+                        TelnetEvent::Subnegotiation(_) => {
+                            todo!();
                         }
                     }
                 }
 
-                println!("Received {bytes_read} bytes: {:02X?}", &network_buffer[..]);
-                network_buffer.clear();
+                network_buffer.advance(bytes_read);
             }
 
             result = stdin.read(&mut stdin_buffer) => {
